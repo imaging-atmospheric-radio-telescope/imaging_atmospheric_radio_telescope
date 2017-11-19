cacd6de70e1e95905e355437ba0232616f266a7e
sebastian <sebmuell@phys.ethz.ch>
2017-11-19 18:59:44 +0100

    make videos of all three field components

--- askarian_telescope/plot.py
+++ askarian_telescope/plot.py
@@ -4,6 +4,7 @@ from matplotlib.patches import RegularPolygon
 from matplotlib.collections import PatchCollection
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import os
+import subprocess
 
 
 def add2ax(
@@ -15,7 +16,6 @@ def add2ax(
     hexrotation=0,
     vmin=None, 
     vmax=None,
-    colorbar_label=None
 ):
     A = pixel_amplitudes
     dx = pixel_directions_x
@@ -50,10 +50,8 @@ def add2ax(
     p.set_clim([vmin, vmax])
 
     divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
+    cax = divider.append_axes('right', size="5%", pad=0.05)
     plt.colorbar(p, cax=cax)
-    if colorbar_label:
-        cax.set_label(colorbar_label)
 
     ax.add_collection(p)
     ax.set_aspect('equal')
@@ -61,44 +59,137 @@ def add2ax(
 
 
 def save_image_slices(
-    out_directory,
-    image_sequence,
-    pixel_directions_x,
-    pixel_directions_y,
+    event, 
+    path,
     time_slice_region_of_interest=np.arange(70,200),
-    colormap='seismic',
-    hexrotation=0,
-    xlabel='x/deg',
-    ylabel='y/deg',
-    colorbar_label='E/(V/m)'
-):
-    os.makedirs(out_directory, exist_ok=True)
+    dpi=160,
+    figsize=(12, 4),
+):  
+    os.makedirs(path, exist_ok=True)
 
-    vmax = image_sequence[:,:].max()
-    vmin = image_sequence[:,:].min()
+    north = event.raw_image_sensor_response.north*1e6 # in uV/m
+    west = event.raw_image_sensor_response.west*1e6
+    vertical = event.raw_image_sensor_response.vertical*1e6
 
-    abs_max = np.max(np.abs([vmax, vmin]))
+    north_max = north[:,:].max()
+    north_min = north[:,:].min()
+    abs_north = np.max(np.abs([north_max, north_min]))
 
-    for time_slice in time_slice_region_of_interest:
-        plt.figure(figsize=(12, 6.75))
-        ax = plt.gca()
-        add2ax(
-            ax=ax, 
-            pixel_amplitudes=image_sequence[:, time_slice], 
-            pixel_directions_x=pixel_directions_x, 
-            pixel_directions_y=pixel_directions_y, 
-            colormap=colormap, 
-            hexrotation=hexrotation, 
-            vmin=abs_max,
-            vmax=-abs_max,
-            colorbar_label=colorbar_label,
+    west_max = west[:,:].max()
+    west_min = west[:,:].min()
+    abs_west = np.max(np.abs([west_max, west_min]))
+
+    vertical_max = vertical[:,:].max()
+    vertical_min = vertical[:,:].min()
+    abs_vertical = np.max(np.abs([vertical_max, vertical_min]))
+
+    pixel_directions_x = np.rad2deg(event.image_sensor.pixel_directions[:, 0])
+    pixel_directions_y = np.rad2deg(event.image_sensor.pixel_directions[:, 1])
+    
+    for idx, time_slice in enumerate(time_slice_region_of_interest):
+
+        fig, axarr = plt.subplots(1, 3, figsize=figsize)
+
+        t = time_slice*event.simulation_truth['time_slice_duration']
+        time_info = 't: '+str(np.round(t*1e9,1))+'ns'
+
+        fig.suptitle(
+            simulation_truth_info_string(event.simulation_truth)+
+            ', '+
+            time_info+'\n'+
+            'north, west, and vertical electric-field /(uV/m)'
         )
-        if xlabel:
-            ax.set_xlabel(xlabel)
-        if ylabel:
-            ax.set_ylabel(ylabel)
+
+        add2ax(
+            ax=axarr[0], 
+            pixel_amplitudes=north[:, time_slice], 
+            pixel_directions_x=pixel_directions_x, 
+            pixel_directions_y=pixel_directions_y,
+            vmin=abs_north, 
+            vmax=-abs_north,
+        )
+        axarr[0].set_xlabel('x/deg')
+        axarr[0].set_ylabel('y/deg')
+        axarr[0].spines['right'].set_visible(False)
+        axarr[0].spines['top'].set_visible(False)
+
+        add2ax(
+            ax=axarr[1], 
+            pixel_amplitudes=west[:, time_slice], 
+            pixel_directions_x=pixel_directions_x, 
+            pixel_directions_y=pixel_directions_y,
+            vmin=abs_west, 
+            vmax=-abs_west,
+        )
+        axarr[1].set_xlabel('x/deg')
+        axarr[1].spines['right'].set_visible(False)
+        axarr[1].spines['top'].set_visible(False)
+        axarr[1].yaxis.set_visible(False)
+
+        add2ax(
+            ax=axarr[2], 
+            pixel_amplitudes=vertical[:, time_slice], 
+            pixel_directions_x=pixel_directions_x, 
+            pixel_directions_y=pixel_directions_y,
+            vmin=abs_vertical, 
+            vmax=-abs_vertical,
+        )
+        axarr[2].set_xlabel('x/deg')
+        axarr[2].spines['right'].set_visible(False)
+        axarr[2].spines['top'].set_visible(False)
+        axarr[2].yaxis.set_visible(False)
+
         plt.savefig(
-            os.path.join(out_directory, "image_{:06d}".format(time_slice)),
-            dpi=160
+            os.path.join(path, "image_{:06d}".format(idx)),
+            dpi=dpi
         )
         plt.close('all')
+
+
+def simulation_truth_info_string(simulation_truth):
+    st = simulation_truth
+    s = ''
+    if st['primary_particle_id'] == 1: s+= 'Gamma'
+    if st['primary_particle_id'] == 14: s+= 'Proton'
+    s+= ', '
+    s+= 'E: '+str(np.round(st['energy']))+'GeV, '
+    s+= 'Az: '+str(np.round(st['azimuth'], 1))+'deg, '
+    s+= 'Zd: '+str(np.round(st['zenith_distance'], 1))+'deg, '
+    s+= 'Core North: '+str(
+            np.round(st['core_position_on_observation_level_north'], 1)
+        )+'m, '
+    s+= 'Core West: '+str(
+            np.round(st['core_position_on_observation_level_west'], 1)
+        )+'m, '
+    s+= 'Obs. altitude: '+str(
+            np.round(st['observation_level_altitude'])
+        )+'m a.s.l'
+    return s
+
+
+def make_video_from_image_slices(
+    image_slice_dir,
+    out_path,
+    image_slice_filename_wildcard='image_%06d.png',
+    fps=25, 
+    threads=1, 
+):
+    """
+    Saves a H264 1080p video of a 3D rendering of a photon-stream event to the 
+    outputpath. In the 3D rendering the event is rotated 360 degrees around the 
+    optical axis of the telescope.
+    """         
+    avconv_command = [
+        'avconv',
+        '-y',  # force overwriting of existing output file
+        '-framerate', str(int(fps)),  # Frames per second
+        '-f', 'image2',
+        '-i', os.path.join(image_slice_dir, image_slice_filename_wildcard),
+        '-c:v', 'h264',
+        #'-s', '1920x1080',  # sample images to FullHD 1080p
+        '-crf', '23',  # high quality 0 (best) to 53 (worst)
+        '-crf_max', '25',  # worst quality allowed
+        '-threads', str(threads),
+        os.path.splitext(out_path)[0] + '.mp4'
+    ]
+    subprocess.call(avconv_command)
