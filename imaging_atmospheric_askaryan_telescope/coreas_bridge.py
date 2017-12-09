791c21f00a0e136fb15cb0d460f7e14e94f5c877
sebastian <sebmuell@phys.ethz.ch>
2017-12-09 08:52:24 +0100

    pep8

--- imaging_atmospheric_askaryan_telescope/coreas_bridge.py
+++ imaging_atmospheric_askaryan_telescope/coreas_bridge.py
@@ -10,19 +10,27 @@ COREAS_VERTICAL_COMPONENT = 3
 
 
 def estimate_time_slice_duration(raw_antenna_time_series):
-    return np.gradient(raw_antenna_time_series[0,:,0]).mean()
+    return np.gradient(raw_antenna_time_series[0, :, 0]).mean()
 
 
-def assert_same_time_slice_duration(raw_antenna_time_series, time_slice_duration):
+def assert_same_time_slice_duration(
+    raw_antenna_time_series,
+    time_slice_duration
+):
     for antenna in range(raw_antenna_time_series.shape[0]):
-        time_slice_duration_this_antenna = np.gradient(raw_antenna_time_series[antenna,:,0])
-        valid = np.abs(time_slice_duration_this_antenna - time_slice_duration) < time_slice_duration/100
+        time_slice_duration_this_antenna = np.gradient(
+            raw_antenna_time_series[antenna, :, 0]
+        )
+        valid = (
+            np.abs(time_slice_duration_this_antenna - time_slice_duration) <
+            time_slice_duration/100
+        )
         assert np.all(valid)
     return time_slice_duration
 
 
 def time_series_paths_in_numerical_order(path):
-    all_time_series_paths = glob.glob(os.path.join(path,'*.dat'))
+    all_time_series_paths = glob.glob(os.path.join(path, '*.dat'))
     antenna_indices = []
     for time_series_path in all_time_series_paths:
         basename = os.path.basename(time_series_path)
@@ -30,20 +38,22 @@ def time_series_paths_in_numerical_order(path):
         antenna_indices.append(antenna_index)
     antenna_indices = np.array(antenna_indices)
     order = np.argsort(antenna_indices)
-    all_time_series_paths = [ all_time_series_paths[i] for i in order]  
+    all_time_series_paths = [all_time_series_paths[i] for i in order]
     return all_time_series_paths
 
 
 CGS_statVolt_per_cm_to_SI_Volt_per_meter = 2.99792458e4
 
+
 def read_all_raw_time_series(path):
     all_time_series_paths = time_series_paths_in_numerical_order(path)
     antenna_responses = []
+    CGS_to_SI = CGS_statVolt_per_cm_to_SI_Volt_per_meter
     for time_series_path in all_time_series_paths:
         time_series = np.genfromtxt(time_series_path, dtype=np.float32)
-        time_series[:,COREAS_NORTH_COMPONENT] *= CGS_statVolt_per_cm_to_SI_Volt_per_meter
-        time_series[:,COREAS_WEST_COMPONENT] *= CGS_statVolt_per_cm_to_SI_Volt_per_meter
-        time_series[:,COREAS_VERTICAL_COMPONENT] *= CGS_statVolt_per_cm_to_SI_Volt_per_meter
+        time_series[:, COREAS_NORTH_COMPONENT] *= CGS_to_SI
+        time_series[:, COREAS_WEST_COMPONENT] *= CGS_to_SI
+        time_series[:, COREAS_VERTICAL_COMPONENT] *= CGS_to_SI
         antenna_responses.append(time_series)
     return np.array(antenna_responses)
 
@@ -53,8 +63,8 @@ def read_electric_field_on_imaging_reflector(path):
     time_slice_duration = estimate_time_slice_duration(raw)
     assert_same_time_slice_duration(raw, time_slice_duration)
 
-    global_start_time = raw[:,:,COREAS_TIME].min()
-    global_end_time = raw[:,:,COREAS_TIME].max()
+    global_start_time = raw[:, :, COREAS_TIME].min()
+    global_end_time = raw[:, :, COREAS_TIME].max()
 
     antenna_start_time_offsets = raw[:, 0, COREAS_TIME] - global_start_time
     antenna_start_slice_offsets = np.round(
