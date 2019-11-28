1f2f264f87f8216e46d46ed3ab1a99cad2a14477
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2019-11-28 17:36:26 +0100

    no more scoop, more general map and reduce

--- imaging_atmospheric_askaryan_telescope/run_simulation.py
+++ imaging_atmospheric_askaryan_telescope/run_simulation.py
@@ -1,33 +1,17 @@
-#!/usr/bin/env python
-# Copyright 2017 Sebastian A. Mueller
-"""
-Usage: run_simulation --out_dir=DIR [--steering_card=PATH] [--corsika_coreas=PATH]
-
-Options:
-    --out_dir=DIR
-    --steering_card=PATH        [default: ./example_steering_card.json]
-    --corsika_coreas=PATH       [default: ../corsika_coreas_build/corsika-75600/run/corsika75600Linux_QGSII_urqmd_coreas]
-"""
-import docopt
-import scoop
 import os
-from os.path import join
 import shutil
-import json
-from imaging_atmospheric_askaryan_telescope import run_corsika_coreas as rcc
-from imaging_atmospheric_askaryan_telescope import telescope
-from imaging_atmospheric_askaryan_telescope import run_utils
+import imaging_atmospheric_askaryan_telescope as iaat
 
 
-def make_event(job):
+def run_job(job):
     try:
         os.makedirs(job['out_run_dir'], exist_ok=True)
-        out_event_dir = join(
-            job['out_run_dir'], 'event_{:06d}'.format(job['event_id'])
-        )
+        out_event_dir = os.path.join(
+            job['out_run_dir'],
+            'event_{:06d}'.format(job['event_id']))
         part_out_event_dir = out_event_dir + '.part'
 
-        rcc.simulate_event(
+        iaat.run_corsika_coreas.simulate_event(
             corsika_coreas_executable_path=job[
                 'corsika_coreas_executable_path'],
             out_event_dir=part_out_event_dir,
@@ -43,8 +27,7 @@ def make_event(job):
                 'core_position_on_observation_level_west'],
             time_slice_duration=job['time_slice_duration'],
             imaging_reflector=job['imaging_reflector'],
-            image_sensor=job['image_sensor'],
-        )
+            image_sensor=job['image_sensor'])
 
         shutil.move(part_out_event_dir, out_event_dir)
     except Exception as e:
@@ -53,58 +36,33 @@ def make_event(job):
     return 1
 
 
-def main():
-    try:
-        arguments = docopt.docopt(__doc__)
-        steering_card_path = arguments['--steering_card']
-        out_run_dir = arguments['--out_dir']
-        corsika_coreas_path = arguments['--corsika_coreas']
-        assert os.path.exists(corsika_coreas_path)
-        with open(steering_card_path, 'rt') as fin:
-            steering_card = json.loads(fin.read())
+def make_jobs(corsika_coreas_path, steering_card, out_dir):
+    assert os.path.exists(corsika_coreas_path)
+    sc = steering_card
 
-        image_sensor = telescope.image_sensor_from_dict(
-            steering_card['image_sensor']
-        )
-        imaging_reflector = telescope.imaging_reflector_from_dict(
-            steering_card['imaging_reflector']
-        )
+    image_sensor = iaat.telescope.image_sensor_from_dict(sc['image_sensor'])
+    imaging_reflector = iaat.telescope.imaging_reflector_from_dict(
+        sc['imaging_reflector'])
 
-        sc = steering_card
-        event_parameters = run_utils.draw_event_parameters(
-            number_events=sc['run']['number_events'],
-            primary_particle_id=sc['run']['primary_particle_id'],
-            energy=sc['run']['energy'],
-            azimuth=sc['run']['azimuth'],
-            zenith_distance=sc['run']['zenith_distance'],
-            observation_level_altitude=sc['run']['observation_level_altitude'],
-            core_position_on_observation_level_max_scatter_radius=sc[
-                'run'][
-                    'core_position_on_observation_level_max_scatter_radius'],
-            time_slice_duration=sc['run']['time_slice_duration'],
-        )
+    event_parameters = iaat.run_utils.draw_event_parameters(
+        number_events=sc['run']['number_events'],
+        primary_particle_id=sc['run']['primary_particle_id'],
+        energy=sc['run']['energy'],
+        azimuth=sc['run']['azimuth'],
+        zenith_distance=sc['run']['zenith_distance'],
+        observation_level_altitude=sc['run']['observation_level_altitude'],
+        core_position_on_observation_level_max_scatter_radius=sc['run'][
+            'core_position_on_observation_level_max_scatter_radius'],
+        time_slice_duration=sc['run']['time_slice_duration'])
 
-        jobs = []
-        for idx in range(sc['run']['number_events']):
-            job = {}
-            for key in event_parameters:
-                job[key] = event_parameters[key][idx]
-            job['out_run_dir'] = out_run_dir
-            job['corsika_coreas_executable_path'] = corsika_coreas_path
-            job['image_sensor'] = image_sensor
-            job['imaging_reflector'] = imaging_reflector
-            jobs.append(job)
-
-        os.makedirs(out_run_dir)
-        shutil.copy(
-            steering_card_path,
-            join(out_run_dir, 'steering_card.json'))
-
-        list(scoop.futures.map(make_event, jobs))
-
-    except docopt.DocoptExit as e:
-        print(e)
-
-
-if __name__ == '__main__':
-    main()
+    jobs = []
+    for idx in range(sc['run']['number_events']):
+        job = {}
+        for key in event_parameters:
+            job[key] = event_parameters[key][idx]
+        job['out_run_dir'] = out_run_dir
+        job['corsika_coreas_executable_path'] = corsika_coreas_path
+        job['image_sensor'] = image_sensor
+        job['imaging_reflector'] = imaging_reflector
+        jobs.append(job)
+    return jobs
