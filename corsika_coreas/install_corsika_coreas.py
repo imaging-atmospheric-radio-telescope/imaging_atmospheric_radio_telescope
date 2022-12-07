5e79484d8295ca299601c0cfd5f125552c9a3840
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2022-12-07 13:30:29 +0100

    split download from install.

--- corsika_coreas/install_corsika_coreas.py
+++ corsika_coreas/install_corsika_coreas.py
@@ -1,29 +1,23 @@
-#!/usr/bin/env python
-# Copyright 2017-2019 Sebastian A. Mueller
-"""
-Install the CORSIKA for the Askarian-telescope.
-
-Usage: install_corsika_coreas -p=PATH --username=USERNAME --password=PASSWORD
-
-Options:
-    -p --install_path=PATH              The path to install CORSIKA in
-    --username=USERNAME                 Username for the KIT-CORSIKA-server
-    --password=PASSWORD                 Password fot the KIT-CORSIKA-server
-
-During the installation, the stdout and stderr of 'coconut_configure'
-and 'coconut_make' are written to the install_path.
-Visit the CORSIKA homepage: https://www.ikp.kit.edu/corsika/
-to test your username and password in the download section.
-If you do not have yet the CORSIKA username and password, drop the developers
-of CORSIKA an e-mail and kindly ask for it.
-"""
-import docopt
-import sys
 import os
 import tarfile
 import shutil
-import glob
 import subprocess
+import glob
+import hashlib
+
+
+CORSIKA_77100_TAR_GZ_HASH_HEXDIGEST = "bc67b14c957a024baf7f2893ab246d34"
+WEB_PATH = "https://web.ikp.kit.edu/corsika/download/old/v771/"
+CORSIKA_NAME = "corsika-77100"
+CORSIKA_TAR_FILENAME = CORSIKA_NAME + ".tar.gz"
+
+
+def md5sum(path):
+    hash_md5 = hashlib.md5()
+    with open(path, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 
 def call_and_save_std(target, o_path, e_path, stdin=None):
@@ -31,34 +25,52 @@ def call_and_save_std(target, o_path, e_path, stdin=None):
         subprocess.call(target, stdout=stdout, stderr=stderr, stdin=stdin)
 
 
-def install(install_path, username, password):
-    corsika_config_path = os.path.abspath("./config.h")
-    assert os.path.isfile(corsika_config_path)
-
-    os.makedirs(install_path, exist_ok=True)
-    os.chdir(install_path)
-
+def download_corsika_tar(
+    output_dir, username, password, web_path, corsika_tar_filename,
+):
     # download CORSIKA from KIT
-    web_path = "https://web.ikp.kit.edu/corsika/download/corsika-v770/"
-    corsika_tarname = "corsika-77100.tar.gz"
-    link = web_path + corsika_tarname
+    # wget uses $http_proxy environment-variables in case of proxy
     call_and_save_std(
-        target=["wget", "--user", username, "--password", password, link],
-        o_path="wget.o",
-        e_path="wget.e",
+        target=[
+            "wget",
+            "--no-check-certificate",
+            "--directory-prefix",
+            output_dir,
+            "--user",
+            username,
+            "--password",
+            password,
+            web_path + corsika_tar_filename,
+        ],
+        stdout_path=os.path.join(
+            output_dir, corsika_tar_filename + ".wget.stdout"
+        ),
+        stderr_path=os.path.join(
+            output_dir, corsika_tar_filename + ".wget.stderr"
+        ),
     )
 
+
+def install(corsika_tar_path, corsika_config_path, install_path):
+    install_path = os.path.abspath(install_path)
+    corsika_config_path = os.path.abspath(corsika_config_path)
+
+    os.makedirs(install_path, exist_ok=True)
+
     # untar, unzip the CORSIKA download
-    tar = tarfile.open(corsika_tarname)
-    tar.extractall(path=".")
+    tar = tarfile.open(corsika_tar_path)
+    tar.extractall(path=install_path)
     tar.close()
 
     # Go into CORSIKA dir
-    corsika_path = os.path.splitext(os.path.splitext(corsika_tarname)[0])[0]
+    corsika_basename = os.path.basename(
+        os.path.splitext(os.path.splitext(corsika_tar_path)[0])[0]
+    )
+    corsika_path = os.path.join(install_path, corsika_basename)
     os.chdir(corsika_path)
 
-    # Provide the Askarian Telescope coconut config.h
-    shutil.copyfile(corsika_config_path, "include/config.h")
+    # Provide the coconut config.h
+    shutil.copyfile(corsika_config_path, os.path.join("include", "config.h"))
 
     # coconut configure
     call_and_save_std(
@@ -79,23 +91,4 @@ def install(install_path, username, password):
     for atmprof in glob.glob("bernlohr/atmprof*"):
         shutil.copy(atmprof, "run")
 
-    if os.path.isfile("run/corsika77100Linux_QGSII_urqmd_coreas"):
-        sys.exit(0)
-    else:
-        sys.exit(1)
-
-
-def main():
-    try:
-        arguments = docopt.docopt(__doc__)
-        install(
-            install_path=arguments["--install_path"],
-            username=arguments["--username"],
-            password=arguments["--password"],
-        )
-    except docopt.DocoptExit as e:
-        print(e)
-
-
-if __name__ == "__main__":
-    main()
+    assert os.path.isfile(os.path.join("run", "corsika77100Linux_QGSII_urqmd_coreas"))
