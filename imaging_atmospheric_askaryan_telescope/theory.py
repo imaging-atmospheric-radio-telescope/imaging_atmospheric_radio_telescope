bea9ecf63de452b18f5fbbcc81e5e0e6503ee832
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-02-15 11:49:28 +0100

    airy disk

--- imaging_atmospheric_askaryan_telescope/theory.py
+++ imaging_atmospheric_askaryan_telescope/theory.py
@@ -2,8 +2,8 @@ import numpy as np
 
 
 def allan_formula(
-    energy_GeV,
-    axis_distance_m,
+    energy_GeV=1e3,
+    axis_distance_m=100,
     zenith_distance_rad=np.deg2rad(30),
     geomagnetic_angle_rad=np.deg2rad(30),
 ):
@@ -17,3 +17,9 @@ def allan_formula(
         * np.cos(zenith_distance_rad)
         * np.exp(-axis_distance_m / R0)
     )
+    return field_uV_per_m_per_MHz
+
+
+def airy_angle(mirror_diameter, wavelength):
+    theta = np.arcsin(1.22 * wavelength / mirror_diameter)
+    return theta
