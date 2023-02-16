558bec9153963d3b22ed810356ecdefb29cdcb6b
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-02-16 11:58:46 +0100

    explore astra2e satellite

---
+++ notebooks/astra_universal/astra2e_satellite.md
@@ -0,0 +1,38 @@
+Source: 2023-02-16, https://www.astra2sat.com/astra-2e-satellite/
+
+Astra 2E Satellite
+==================
+
+astra2e-posterSatellite Name: Astra 2E
+Status: Operational
+Position: 28.2° East
+Norad: 39285
+Cospar number: 2013-056A
+Operator: SES
+Launch date: September 30, 2013
+Launch site: Baikonur Cosmodrome
+Launch vehicle: Proton Breeze M
+Launch mass (kg): Total payload lift performance more than 10,200 kg
+Dry mass (kg): NOT KNOWN
+Solar Array Span: 40 m
+Manufacturer: EADS Astrium
+Platform: Eurostar-E3000
+Coverage: Europe, the Middle East and Africa
+Beams: UK Spotbeam, Pan-European Beam, Middle East Spotbeam
+Bands: KU and KA Power (EOL): 13 kw
+Number of Transponders: Ku-Band 60, Ka-Band 3
+Orbit: Geostationary
+Beacon Frequency: 11.707 – Polarity L/R Expected lifetime: 15 years
+
+ 
+
+Operational Frequency Bands
+---------------------------
+
+Uplink: 12.75-13.00 GHz, 13.16-13.25 GHz, 13.75-14.50 GHz, 17.30-18.35 GHz, 27.85-28.45 GHz, 28.52-28.76 GHz, 28.90-30.00 GHz
+Downlink: 10.70-1 1.20 GHz, 11.36-12.75 GHz, 18.85-20.20 GHz, 21.40-22.20 GHz
+
+TT&C Frequency Bands
+
+Uplink: 17309.5 MHz, 18090.0 MHz
+Downlink: 1 1707.5 MHz, 12493.0 MHz, 1 1453.5 MHz, 18800.5 MHz
---
+++ notebooks/astra_universal/estimate_electric_field_of_ASTRA_UNIVERSAL_satellite.py
@@ -0,0 +1,47 @@
+"""
+How strong is the electric field seen by a comercial lnb of the ASTRA-universal
+satellite?
+"""
+
+# Estimate the max. areal power density ASTRA creates on earth
+vacuum_impedance_ohm = 120 * np.pi
+speed_of_light_m_per_s = 299792458.0
+
+astra2e_local_oscillator_frequency_Hz = 9.75e9
+astra2e_wavelength_m = speed_of_light_m_per_s / astra2e_local_oscillator_frequency_Hz
+astra2e_antenna_effective_area_m2 = (astra2e_wavelength_m ** 2) / (4.0 * np.pi)
+
+astra2e_spots_m2 = {}
+astra2e_spots_m2["kaband"] = (np.pi/4) * 3236e3 * 1640e3 # from astra2e_kaband.svg
+astra2e_spots_m2["efg-pe"] = (np.pi/4) * 3569e3 * 2890e3 # from astra2e_efg-pe.svg
+astra2e_spots_m2["me"] = (np.pi/4) * 3980e3 * 3543e3 # from astra2e_me.svg
+astra2e_spots_m2["efg-ukspot"] = (np.pi/4) * 2024e3 * 1415e3 # from astra2efg-ukspot.svg
+
+astrea2e_area_m2 = np.sum([astra2e_spots_m2[k] for k in astra2e_spots_m2])
+astra2e_max_power_W = 13.0e3 # from astra2e_satellite.md
+
+astra2e_power_density_on_surface_of_earth_W_per_m2 = (
+    astra2e_max_power_W / astrea2e_area_m2
+)
+
+typical_satellite_dish_area_m2 = 0.4 ** 2 * np.pi
+feedhorn_transmission = 1.0 # <- unrealistic, more typical value is 0.5.
+
+power_arriving_in_lnb_W = (
+    astra2e_power_density_on_surface_of_earth_W_per_m2 *
+    feedhorn_transmission *
+    typical_satellite_dish_area_m2
+)
+
+electric_field_V_per_m = np.sqrt(
+    (power_arriving_in_lnb_W * vacuum_impedance_ohm) /
+    astra2e_antenna_effective_area_m2
+)
+# ohm = (V ** 2 / W)
+
+
+print(
+    "The ASTRA-lnb installed in a typical 80cm dish experiences "
+    "an electric field of: "
+    "{:.2f}uVm^{{-1}}".format(1e6 * electric_field_V_per_m)
+)
