47b0199dbd12e4a1450ba8b65c7b8110e3f16164
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-05-08 16:43:44 +0200

    keep using simple propagation method for now.

--- imaging_atmospheric_askaryan_telescope/production/__init__.py
+++ imaging_atmospheric_askaryan_telescope/production/__init__.py
@@ -85,7 +85,7 @@ def simulate_telescope_response(
                 path=os.path.join(out_dir, "mirror", "electric_fields.tar"),
             )
             E_sensor = (
-                simtelescope.propagate_electric_field_from_mirror_to_sensor2(
+                simtelescope.propagate_electric_field_from_mirror_to_sensor(
                     telescope=telescope,
                     mirror_electric_fields=E_mirror,
                     num_time_slices=timing["electric_fields"]["sensor"][
