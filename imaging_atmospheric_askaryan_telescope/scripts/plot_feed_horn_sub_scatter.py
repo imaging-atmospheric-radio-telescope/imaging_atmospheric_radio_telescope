7c6e16749c6b30b415a219c2de1b8aa84fb30e54
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-05-13 12:34:19 +0200

    rm obsolete

--- imaging_atmospheric_askaryan_telescope/scripts/plot_feed_horn_sub_scatter.py
+++ imaging_atmospheric_askaryan_telescope/scripts/plot_feed_horn_sub_scatter.py
@@ -23,42 +23,14 @@ parser.add_argument(
 sebplt.matplotlib.rcParams.update(iaat_plot.CONFIG["matplotlib"])
 sebplt.matplotlib.rcParams["text.latex.preamble"] = r"\usepackage{amsmath}"
 
-
 args = parser.parse_args()
 out_dir = args.out_dir
 os.makedirs(out_dir, exist_ok=True)
 
-inner_radius_m = 0.2
-for num in [1, 3, 4, 5, 6, 7]:
-
-    print(num)
-    fig = sebplt.figure(style={"rows": 1280, "cols": 1280, "fontsize": 1.5})
-    ax = sebplt.add_axes(fig=fig, span=[0.15, 0.15, 0.65, 0.65])
-    sebplt.ax_add_hexagon(
-        ax=ax,
-        x=0.0,
-        y=0.0,
-        r_outer=np.sqrt(3) / 2 * inner_radius_m,
-        orientation_deg=30,
-        color="black",
-        linewidth=0.2,
-    )
-    xy = iaat.telescope.make_feed_horn_sub_scatter(
-        num=num, inner_radius_m=inner_radius_m
-    )
-    print(xy)
-    ax.plot(xy[:, 0], xy[:, 1], marker="o", color="red", linewidth=0.0)
-    ax.set_xlabel("x / m")
-    ax.set_ylabel("y / m")
-    ax.set_aspect("equal")
-    fig.savefig(os.path.join(out_dir, f"{num:d}.jpg"))
-    sebplt.close(fig)
-
-
 screen_radius_m = 0.1
 feed_horn_inner_radius_m = 0.03
 focal_ratio_1 = 1.4
-feed_horn_oversampling = 2
+feed_horn_oversampling = 1
 
 cc = iaat.camera.make_camera(
     sensor_outer_radius_m=screen_radius_m,
